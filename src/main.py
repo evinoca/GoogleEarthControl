30aa10df3243566ac69252edb75305eec90019ce
Audric <evinoca@gmail.com>
2015-07-16 10:55:38 +0800

    tweek module struct

---
+++ src/main.py
@@ -0,0 +1,9 @@
+#encoding: utf-8
+'''
+Created on 2014-2-20
+@author: Audric
+
+Here i want to build a basic toy which is to detect all the fish in a pool or a lake.
+Try not to fail a friend's wish.
+
+'''
--- src/mech_platform/bodyController.py
+++ src/mech_platform/bodyController.py
@@ -1,13 +1,4 @@
-# -*- coding: cp936 -*-
-'''
-Created on 2014-2-20
-@author: Audric
-
-Here i want to build a basic toy which is to detect all the fish in a pool or a lake.
-Try not to fail a friend's wish.
-
-'''
-
+#encoding: utf-8
 
 
 
